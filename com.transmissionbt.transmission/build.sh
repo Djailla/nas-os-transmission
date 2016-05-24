9559de8e40daafb171402232795a83bb77a947dd
Djailla <bastien.vallet@gmail.com>
2016-05-24 11:17:34 +0200

    First drop

---
+++ com.transmissionbt.transmission/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+/bin/echo -e '#!/bin/sh\nexit 101' > /usr/sbin/policy-rc.d
+chmod +x /usr/sbin/policy-rc.d
+
+apt-get update
+apt-get install -y -q --no-install-recommends transmission-daemon
+
+rm /usr/sbin/policy-rc.d
+
+mkdir -p /var/run/transmission-daemon
+chown debian-transmission:debian-transmission /var/run/transmission-daemon
+
+install -m 755 /home/source/rc.local /etc
+install -m 755 /home/source/settings.json /etc/transmission-daemon
+install -m 755 /home/source/etc/default/transmission-daemon /etc/default
+install -m 755 /home/source/etc/init.d/transmission-daemon /etc/init.d/
+
+
+exit 0
---
+++ com.transmissionbt.transmission/scripts/config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import json
+import sys
+
+TRANSMISSION_SETTINGS = '/etc/transmission-daemon/settings.json'
+
+if len(sys.argv) < 3:
+    sys.exit(1)
+
+with open(TRANSMISSION_SETTINGS, 'r') as settings_file:
+    settings = json.load(settings_file)
+
+if sys.argv[1] not in settings:
+    sys.exit(1)
+
+value_type = type(settings[sys.argv[1]])
+settings[sys.argv[1]] = value_type(sys.argv[2])
+
+with open(TRANSMISSION_SETTINGS, 'w') as settings_file:
+    json.dump(settings, settings_file, indent=4)
+
