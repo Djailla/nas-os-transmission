374f11eace9f148e9c5f7537e19598076461c7e3
Djailla <bastien.vallet@gmail.com>
2016-12-18 22:49:25 +0100

    Remove custom configuration for now.

--- com.transmissionbt.transmission/build.sh
+++ com.transmissionbt.transmission/build.sh
@@ -4,9 +4,9 @@
 chmod +x /usr/sbin/policy-rc.d
 
 apt-get update
-apt-get install -y -q --no-install-recommends transmission-daemon python-pip
+apt-get install -y -q --no-install-recommends transmission-daemon
 
-pip install flask
+# pip install flask
 
 rm /usr/sbin/policy-rc.d
 
@@ -14,13 +14,13 @@ mkdir -p /var/run/transmission-daemon
 chown debian-transmission:debian-transmission /var/run/transmission-daemon
 
 install -m 755 /home/source/rc.local /etc
-install -m 755 /home/source/settings.json /var/lib/transmission-daemon/info
+install -m 755 /home/source/settings.json /etc/transmission-daemon/
 install -m 755 /home/source/etc/default/transmission-daemon /etc/default
 install -m 755 /home/source/etc/init.d/transmission-daemon /etc/init.d/
 
-mkdir -m 755 -p /opt/transmission/app
-mkdir -m 755 -p /opt/transmission/scripts
-cp -r /home/source/app/* /opt/transmission/app/
-cp -r /home/source/scripts/* /opt/transmission/scripts/
+# mkdir -m 755 -p /opt/transmission/app
+# mkdir -m 755 -p /opt/transmission/scripts
+# cp -r /home/source/app/* /opt/transmission/app/
+# cp -r /home/source/scripts/* /opt/transmission/scripts/
 
 exit 0
